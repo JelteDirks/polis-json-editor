074eb71b07357fd157eb06b842e8f5b49b4c5ccf
Jelte Dirks <j.dirks@outlook.com>
2024-03-21 21:36:31 +0100

    show the file that is currently being edited
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/handlers/chooseMode.js
+++ src/handlers/chooseMode.js
@@ -5,8 +5,11 @@ export async function chooseMode(internalState) {
 
   clearView()
 
-  process.stdout.write("i: Ik wil nieuwe regel objecten toevoegen (insert mode)");
-  process.stdout.write("\ne: Ik wil bestaande regel objecten wijzigen (edit mode)");
+  process.stdout.write("Je bent het onderstaande bestand aan het aanpassen\n" +
+    internalState.argv.file);
+
+  process.stdout.write("\ni: Ik wil nieuwe regel objecten toevoegen (insert mode)");
+  process.stdout.write("\ne: Ik wil een bestaand regel object wijzigen (edit mode)");
   process.stdout.write("\nWat wil je doet met het bestand? (default = i)\n");
 
   const answer = await readOne();
